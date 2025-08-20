5cc21f3711287f85ca5214e81df6836364a520b8
TangoBalango <153786566+TangoBalango@users.noreply.github.com>
2025-08-20 00:56:46 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Pessoa {
+    protected String nome;
+    protected String email;
+
+public Pessoa(String nome, String email){
+    this.email = email;
+    this.nome = nome;
+}
+
+public String getNome(){
+    return nome;
+}
+public String getEmail(){
+    return email;
+}
+
+
+
+
+
+
+
+
+
+
+}
